f049bbb662b6af901c5d4db38c693c6c50afda74
jjpvoskuil <jjpvoskuil@gmail.com>
2026-06-03 12:53:12 -0500

    Update 4_MS_Financial_Modeler.py

--- pages/4_MS_Financial_Modeler.py
+++ pages/4_MS_Financial_Modeler.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(
-    page_title="MS Financial Modeler | Voskuil FP",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-)
 
 # Hide Streamlit chrome to give the iframe maximum space
 st.markdown("""
